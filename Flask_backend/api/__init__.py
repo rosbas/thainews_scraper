596e2cc625c5fd0ed06663f82eb754a8a74d2043
Rosbas <panutatr@hotmail.com>
2020-06-29 11:53:58 +0700

    add one line solve CORS problem

--- Flask_backend/api/__init__.py
+++ Flask_backend/api/__init__.py
@@ -7,13 +7,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app
-         # , supports_credentials=True
+    CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}}
          )
 
     # //for scrapy
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scrapy_quotes.db'
-    # app.config['CORS_HEADERS'] = 'Content-Type'
+    app.config['CORS_HEADERS'] = 'Content-Type'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
     db.init_app(app)
